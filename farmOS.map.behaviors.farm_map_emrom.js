06a27f2268ef823a57547d851c2b40efa35c8e76
bonushenricus <62553299+bonushenricus@users.noreply.github.com>
2020-04-10 22:36:28 +0200

    Add files via upload

---
+++ farmOS.map.behaviors.farm_map_emrom.js
@@ -0,0 +1,73 @@
+(function () {
+  farmOS.map.behaviors.farm_map_emrom = {
+    attach: function (instance) {
+      var suolo50k = {
+        title: 'delineazioni del suolo',
+        url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
+          params: {
+	    LAYERS: 'Carta_Suoli_50k',
+	    VERSION: '1.1.1',
+	    FORMAT: 'image/png8',
+	  },
+	  visible: true, // defaults to true
+	  base: false, // defaults to false
+        group: 'Base layers',
+      };
+      var sostorg = {
+        title: 'sostanza organica',
+        url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
+          params: {
+	    LAYERS: 'Dotazione_in_sostanza_organica',
+	    VERSION: '1.1.1',
+	    FORMAT: 'image/png',
+	  },
+	  visible: false, // defaults to true
+	  base: false, // defaults to false
+	  opacity: 0.5,
+        group: 'Base layers',
+      };
+      var capuso = {
+        title: 'capacità uso del suolo',
+        url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
+          params: {
+	    LAYERS: 'Capacita_uso_suoli',
+	    VERSION: '1.1.1',
+	    FORMAT: 'image/png',
+	  },
+	  visible: false, // defaults to true
+	  base: false, // defaults to false
+	  opacity: 0.5,
+        group: 'Base layers',
+      };
+      var ctr = {
+        title: 'carta tecnica regionale',
+        url: 'http://servizigis.regione.emilia-romagna.it/wms/dbtr_ctrmultiscala',
+          params: {
+	    LAYERS: 'DBTR_CtrMultiscala',
+	    VERSION: '1.1.1',
+	    FORMAT: 'image/png8',
+	  },
+	  visible: false, // defaults to true
+	  base: false, // defaults to false
+        group: 'Base layers',
+      };
+      var frane = {
+        title: 'coperture quaternarie e frane',
+        url: 'http://servizigis.regione.emilia-romagna.it/wms/geologia10k',
+          params: {
+	    LAYERS: 'Coperture_quaternarie_10k',
+	    VERSION: '1.1.1',
+	    FORMAT: 'image/png8',
+	  },
+	  visible: false, // defaults to true
+	  base: false, // defaults to false
+        group: 'Base layers',
+      };
+      instance.addLayer('wms', capuso);
+      instance.addLayer('wms', sostorg);
+      instance.addLayer('wms', suolo50k);
+      instance.addLayer('wms', ctr);
+      instance.addLayer('wms', frane);
+    },
+  };
+}());
