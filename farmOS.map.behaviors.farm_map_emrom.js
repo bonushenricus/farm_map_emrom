d1bc68affe358318452fa387393f24c1c767d4b6
bonushenricus <62553299+bonushenricus@users.noreply.github.com>
2020-04-12 08:27:04 +0200

    Add files via upload

--- farmOS.map.behaviors.farm_map_emrom.js
+++ farmOS.map.behaviors.farm_map_emrom.js
@@ -6,62 +6,62 @@
         url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
           params: {
 	    LAYERS: 'Carta_Suoli_50k',
-	    VERSION: '1.1.1',
+	    VERSION: '1.3.0',
 	    FORMAT: 'image/png8',
 	  },
 	  visible: true, // defaults to true
 	  base: false, // defaults to false
-        group: 'Base layers',
+        group: 'Emilia-Romagna',
       };
       var sostorg = {
         title: 'sostanza organica',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
           params: {
 	    LAYERS: 'Dotazione_in_sostanza_organica',
-	    VERSION: '1.1.1',
+	    VERSION: '1.3.0',
 	    FORMAT: 'image/png',
 	  },
 	  visible: false, // defaults to true
 	  base: false, // defaults to false
-	  opacity: 0.5,
-        group: 'Base layers',
+  	  opacity: 0.5,
+        group: 'Emilia-Romagna',
       };
       var capuso = {
         title: 'capacità uso del suolo',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
           params: {
 	    LAYERS: 'Capacita_uso_suoli',
-	    VERSION: '1.1.1',
+	    VERSION: '1.3.0',
 	    FORMAT: 'image/png',
 	  },
 	  visible: false, // defaults to true
 	  base: false, // defaults to false
 	  opacity: 0.5,
-        group: 'Base layers',
+        group: 'Emilia-Romagna',
       };
       var ctr = {
         title: 'carta tecnica regionale',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/dbtr_ctrmultiscala',
           params: {
 	    LAYERS: 'DBTR_CtrMultiscala',
-	    VERSION: '1.1.1',
+	    VERSION: '1.3.0',
 	    FORMAT: 'image/png8',
 	  },
 	  visible: false, // defaults to true
 	  base: false, // defaults to false
-        group: 'Base layers',
+        group: 'Emilia-Romagna',
       };
       var frane = {
         title: 'coperture quaternarie e frane',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/geologia10k',
           params: {
 	    LAYERS: 'Coperture_quaternarie_10k',
-	    VERSION: '1.1.1',
+	    VERSION: '1.3.0',
 	    FORMAT: 'image/png8',
 	  },
 	  visible: false, // defaults to true
 	  base: false, // defaults to false
-        group: 'Base layers',
+        group: 'Emilia-Romagna',
       };
       instance.addLayer('wms', capuso);
       instance.addLayer('wms', sostorg);
