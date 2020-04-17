0f25a7631c79334db3c9517a43c53e7efd914c31
bonushenricus <62553299+bonushenricus@users.noreply.github.com>
2020-04-17 10:01:51 +0200

    Add files via upload

--- farmOS.map.behaviors.farm_map_emrom.js
+++ farmOS.map.behaviors.farm_map_emrom.js
@@ -4,6 +4,7 @@
       var suolo50k = {
         title: 'delineazioni del suolo',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
+	attribution: '<a href="https://agri.regione.emilia-romagna.it/Suoli/jsp/common/pannelloDelineazione.jspx?delin_id=9768">(url delin_id=delineazione) CC-BY 3.0 Serv. geologico, sismico e dei suoli - Reg. Emilia-Romagna</a>',
           params: {
 	    LAYERS: 'Carta_Suoli_50k',
 	    VERSION: '1.3.0',
@@ -11,11 +12,12 @@
 	  },
 	  visible: true, // defaults to true
 	  base: false, // defaults to false
-        group: 'Emilia-Romagna',
+          group: 'Emilia-Romagna',
       };
       var sostorg = {
         title: 'sostanza organica',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
+        attribution: '<a href="http://servizigis.regione.emilia-romagna.it/wms/suoli?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=Dotazione_in_sostanza_organica">(url legenda) CC-BY 3.0 Serv. geologico, sismico e dei suoli - Reg. Emilia-Romagna</a>',
           params: {
 	    LAYERS: 'Dotazione_in_sostanza_organica',
 	    VERSION: '1.3.0',
@@ -29,6 +31,7 @@
       var capuso = {
         title: 'capacità uso del suolo',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/suoli',
+        attribution: '<a href="https://ambiente.regione.emilia-romagna.it/it/geologia/suoli/pdf/scheda_capacita_uso.pdf/@@download/file/scheda_capacita_uso.pdf">(url doc pdf) CC-BY 3.0 Serv. geologico, sismico e dei suoli - Reg. Emilia-Romagna</a>',
           params: {
 	    LAYERS: 'Capacita_uso_suoli',
 	    VERSION: '1.3.0',
@@ -42,6 +45,7 @@
       var ctr = {
         title: 'carta tecnica regionale',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/dbtr_ctrmultiscala',
+        attribution: '<a href="http://servizigis.regione.emilia-romagna.it/wms/dbtr_ctrmultiscala?request=GetCapabilities&service=WMS">(url capabilities) CC-BY 3.0 Serv. Stat., Comunic., S.I.G., partecipazione - Reg. Emilia-Romagna</a>',
           params: {
 	    LAYERS: 'DBTR_CtrMultiscala',
 	    VERSION: '1.3.0',
@@ -54,6 +58,7 @@
       var frane = {
         title: 'coperture quaternarie e frane',
         url: 'http://servizigis.regione.emilia-romagna.it/wms/geologia10k',
+        attribution: '<a href="http://servizigis.regione.emilia-romagna.it/wms/geologia10k?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=Coperture_quaternarie_10K">(url legenda) CC-BY 3.0 Serv. geologico, sismico e dei suoli - Reg. Emilia-Romagna</a>',
           params: {
 	    LAYERS: 'Coperture_quaternarie_10k',
 	    VERSION: '1.3.0',
